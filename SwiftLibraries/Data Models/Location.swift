94366900acfbb25b25d83191b31f9b8c563f9c79
Allan Evans <lingoslinger@me.com>
2018-12-12 09:08:58 -0600

    Swift 4.2 updates part one: structs replace data model classes

---
+++ SwiftLibraries/Data Models/Location.swift
@@ -0,0 +1,27 @@
+//
+//	Location.swift
+//	Model file generated using JSONExport: https://github.com/Ahmed-Ali/JSONExport
+
+import Foundation
+
+struct Location : Codable {
+
+	let latitude : String?
+	let longitude : String?
+	let needsRecoding : Bool?
+
+
+	enum CodingKeys: String, CodingKey {
+		case latitude = "latitude"
+		case longitude = "longitude"
+		case needsRecoding = "needs_recoding"
+	}
+	init(from decoder: Decoder) throws {
+		let values = try decoder.container(keyedBy: CodingKeys.self)
+		latitude = try values.decodeIfPresent(String.self, forKey: .latitude)
+		longitude = try values.decodeIfPresent(String.self, forKey: .longitude)
+		needsRecoding = try values.decodeIfPresent(Bool.self, forKey: .needsRecoding)
+	}
+
+
+}
--- SwiftLibraries/View Controllers/LibraryDetailViewController.swift
+++ SwiftLibraries/View Controllers/LibraryDetailViewController.swift
@@ -17,27 +17,28 @@ class LibraryDetailViewController: UIViewController {
     @IBOutlet weak var libraryHoursLabel: UILabel!
     @IBOutlet weak var libraryMapView: MKMapView!
     
-    var detailLibrary : Library!
+    var detailLibrary : Library?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = detailLibrary.libraryName
-        libraryPhoneTextView.text = "Phone: \(detailLibrary.phone!)"
-        libraryAddressLabel.text = detailLibrary.address
-        libraryHoursLabel.text = detailLibrary.hoursOfOperation.formattedLibraryHours
+        title = detailLibrary?.name ?? "Library name not available"
+        let phone = detailLibrary?.phone ?? "Library phone unavailable"
+        libraryPhoneTextView.text = "Phone: \(phone)"
+        libraryAddressLabel.text = detailLibrary?.address ?? "Library address unavailable"
+        libraryHoursLabel.text = detailLibrary?.hoursOfOperation?.formattedLibraryHours ?? "Library hours unavailable"
         annotateMap()
     }
     
     
     func annotateMap() {
-        let zoomLocation = CLLocationCoordinate2D.init(latitude: (detailLibrary.location.latitude as NSString).doubleValue,
-                                                       longitude: (detailLibrary.location.longitude as NSString).doubleValue)
+        let latitudeString = detailLibrary?.location?.latitude ?? ""
+        let longitudeString = detailLibrary?.location?.longitude ?? ""
+        let zoomLocation = CLLocationCoordinate2D.init(latitude: Double(latitudeString) ?? 0.0, longitude: Double(longitudeString) ?? 0.0)
         let span = MKCoordinateSpan.init(latitudeDelta: 0.01, longitudeDelta: 0.01)
         let viewRegion = MKCoordinateRegion.init(center: zoomLocation, span: span)
-        
         let point = MKPointAnnotation.init()
         point.coordinate = zoomLocation
-        point.title = detailLibrary.libraryName
+        point.title = detailLibrary?.name
         libraryMapView.addAnnotation(point)
         libraryMapView.setRegion(libraryMapView.regionThatFits(viewRegion), animated: true)
     }
